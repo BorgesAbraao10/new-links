e1308805ce0baaedbd4b231d90f1bedc9e2c13fe
BorgesAbraao10 <abraao10clear@gmail.com>
2024-09-16 19:54:03 -0300

    Last update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,6 @@
+import { Social } from "../../components/Social";
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
+
 export function Home() {
   return (
     <div className="flex flex-col w-full py-4 items-center justify-center  ">
@@ -8,15 +11,24 @@ export function Home() {
 
       <main className="flex flex-col w-11/12 max-w-xl text-center">
         <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
-          <a
-            href="https://music.youtube.com/watch?v=TsOtMihNrCI&list=OLAK5uy_lEQUj31p6LNMYEMsBr6v2Y3uCS8HJT9Og"
-            target="_blank"
-          >
+          <a href="#">
             <p className="text-base md:text-lg">Canal no Youtube</p>
           </a>
         </section>
 
-        <footer className="flex justify-center gap-3 my-4"></footer>
+        <footer className="flex justify-center gap-3 my-4">
+          <Social url="https://pt-br.facebook.com/">
+            <FaFacebook size={35} color="#FFF" />
+          </Social>
+
+          <Social url="https://www.instagram.com/?hl=pt-br">
+            <FaInstagram size={35} color="#FFF" />
+          </Social>
+
+          <Social url="https://www.youtube.com/">
+            <FaYoutube size={35} color="#FFF" />
+          </Social>
+        </footer>
       </main>
     </div>
   );
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 export function Login() {
   return (
-    <div className="w-full h-screen bg-slate-950">
-      <h1 className="text-white text-3xl">Página de Login</h1>
+    <div className="flex w-full h-screen items-center justify-center flex-col">
+      <Link to="/">
+        <h1 className="mt-11 text-white mb-7 font-bold text-5xl">
+          Dev<span className="bg-gradient-to-r from-yellow-500">LinK</span>
+        </h1>
+      </Link>
     </div>
   );
 }
