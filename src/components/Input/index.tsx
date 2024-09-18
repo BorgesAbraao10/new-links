e1cc1ad5831eedd654ff4e301c1c1d8bd56865f0
BorgesAbraao10 <abraao10clear@gmail.com>
2024-09-17 22:00:32 -0300

    Last update

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,6 @@
 import { InputHTMLAttributes } from "react";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
-
-export function Input(props: InputProps) {
+export function Input(props: InputHTMLAttributes<HTMLInputElement>) {
   return (
     <input
       className="border-0 h-9 rounded-md outline-none px-2 mb-2"
