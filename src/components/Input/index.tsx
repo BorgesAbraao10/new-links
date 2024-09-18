1e5e0657a39cad1f149abb616045937efa892312
BorgesAbraao10 <abraao10clear@gmail.com>
2024-09-17 21:51:17 -0300

    Last update

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,12 @@
-export function Input() {
+import { InputHTMLAttributes } from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export function Input(props: InputProps) {
   return (
     <input
       className="border-0 h-9 rounded-md outline-none px-2 mb-2"
-      placeholder="Digite seu texto aqui..."
+      {...props}
     />
   );
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,20 @@
+import { FormEvent, useState } from "react";
+
 import { Link } from "react-router-dom";
 import { Input } from "../../components/Input";
 
 export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    console.log({
+      email: email,
+      password: password,
+    });
+  }
+
   return (
     <div className="flex w-full h-screen items-center justify-center flex-col">
       <Link to="/">
@@ -10,8 +23,24 @@ export function Login() {
         </h1>
       </Link>
 
-      <form className="w-full max-w-xl flex flex-col px-2">
-        <Input />
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-xl flex flex-col px-2"
+      >
+        <Input
+          placeholder="Digite o seu e-mail..."
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <Input
+          placeholder="*********************"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
         <button
           type="submit"
           className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white "
